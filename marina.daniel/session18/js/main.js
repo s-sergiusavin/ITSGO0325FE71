db102be57e62689b17bd8a9500f9cfced4fc1661
MDani94 <ave_avenger@yahoo.com>
2025-05-15 19:54:55 +0300

    palindrom test

--- marina.daniel/session18/js/main.js
+++ marina.daniel/session18/js/main.js
@@ -305,7 +305,7 @@ if (valoriMoneda[Math.round(Math.random())] === 'cap') {
 }
 
 // Palindrome game
-debugger;
+
 const palindrome = [1, 9, 7, 8, 7, 9, 1];
 const checkPalindrome = (array) => {
     for (let i = 0; i < array.length / 2; i++) {
@@ -321,6 +321,28 @@ const checkPalindrome = (array) => {
 
 console.log(checkPalindrome(palindrome));
 // checkPalindrome(palindrome)
+
+
+function isPalindrome(str) {
+    let rev = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        rev += str[i];
+    }
+    if (rev == str) {
+        return true
+    } else {
+        return false;
+    }
+}
+
+let str1 = "racecar";
+let str2 = "nitin";
+let str3 = "Rama";
+
+console.log(isPalindrome(str1));
+console.log(isPalindrome(str2));
+console.log(isPalindrome(str3));
+
 
 
 
