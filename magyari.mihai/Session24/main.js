90285272fbbcb1d82d68c75309ed42f88b1411b5
magyarimihai <magyari.mihaly15@gmail.com>
2025-06-07 12:30:48 +0300

    modified session 24

--- magyari.mihai/Session24/main.js
+++ magyari.mihai/Session24/main.js
@@ -13,21 +13,17 @@
  */
 
 /**
- * HTTP Implementation of rest services
+ * HTTP implementation of rest services
  * Create => POST
  * Read => GET
- * Update => PUT / PATCH (update partial)
+ * Update => PUT/ PATCH (update partial)
  * Delete => DELETE
  */
 
-
-
-
-// const myFirstPromise = new Promise(function (resolve, reject) {
+// const myFirstPromise = new Promise( function(resolve, reject) {
 //     // setTimeout(resolve, 3000, 'Value sent!!!')
 //     setTimeout(reject, 3000, 'Error sent!!!')
-
-// })
+// });
 
 // console.log(myFirstPromise);
 
@@ -40,7 +36,6 @@
 //     }
 // );
 
-
 const apiConfig = {
     headers: {
         'x-api-key': 'reqres-free-v1'
@@ -48,7 +43,7 @@ const apiConfig = {
 }
 
 /**
- * GET Request
+ * GET request
  */
 
 const singleUserUrl = 'https://reqres.in/api/users/2';
@@ -61,55 +56,47 @@ const singleUserUrl = 'https://reqres.in/api/users/2';
 // });
 
 
-// console.log(`------`);
-
 // const singleUserData = fetch(singleUserUrl, apiConfig)
 //     .then(response => {
-//         console.log(`------`);
-//         return response.json()
+//         console.log('----');
+//        return response.json()
 //     });
 
-
-// singleUserData.then(data => console.log(data));
-
+// singleUserData.then( data => console.log(data));
 
 /**
  * Async/await request
  */
 
-
 async function getSingleUser() {
-    const response = await fetch(singleUserUrl, apiConfig)
+    const response = await fetch(singleUserUrl, apiConfig);
     return response.json()
 }
 
 getSingleUser().then(data => {
     console.log('Data with async/await');
     console.log(data);
-})
+});
 
 /**
  * Get request with error handling
  */
 
-
 const singleUserNotFoundUrl = 'https://reqres.in/api/users/23';
 const userListUrl = 'https://reqres.in/api/users?page=2';
 
-// semnul intrebarii (?) in url se numeste query param
-//query param este o pereche cheie=valoare
-//cu mai multi parametrii primul va avea ? in fata si vor fi separati de &
-//?cheie=valoare1&cheie2=valoare2
-
+// ? in url se numeste query param
+// query param este o pereche cheie=valoare
+// cu mai multi parametrii primul va avea ? in fata si vor fi separati de &
+// ?cheie1=valoare1&cheie2=valoare2
 
 const allUserList = new Promise((resolve, reject) => {
-    fetch(singleUserNotFoundUrl, apiConfig).then(
+    fetch(userListUrl, apiConfig).then(
         response => {
             console.log(response)
             if (response.status !== 200) {
                 throw 'Ai o eroare';
             }
-
             return response.json()
         }
     ).then(data => {
@@ -126,19 +113,17 @@ allUserList.then(
     error => {
         console.error(error)
     }
-)
-
+);
 
 /**
  * POST request
  */
 
 const createUserUrl = 'https://reqres.in/api/users';
-
 const user = {
     "name": "morpheus",
     "job": "leader"
-};
+}
 
 const createUser = new Promise((resolve, reject) => {
     fetch(createUserUrl, {
@@ -148,7 +133,7 @@ const createUser = new Promise((resolve, reject) => {
             'x-api-key': 'reqres-free-v1'
         },
         body: JSON.stringify(user)
-    }) // pana in punctul acesta se face requestul
+    }) // pana in punctul asta se face requestul
         .then(response => {
             console.log(response)
             if (response.status !== 201) {
@@ -157,13 +142,12 @@ const createUser = new Promise((resolve, reject) => {
             return response.json();
         }).then(data => resolve(data))
         .catch(error => reject(error))
-
-})
+});
 
 createUser.then(
     value => console.log(value),
-    error => console.error(error),
-)
+    error => console.error(error)
+);
 
 /**
  * POST request with error
@@ -181,11 +165,11 @@ const userSuccessful = {
 const registerUrlConfig = {
     method: 'POST',
     headers: {
-        'Content-Type': 'application/JSON',
+        'Content-Type': 'application/json',
         'x-api-key': 'reqres-free-v1'
     },
-    body: JSON.stringify(userIncomplete)
-    // body: JSON.stringify(userSuccessful)
+    // body: JSON.stringify(userIncomplete),
+    body: JSON.stringify(userSuccessful),
 }
 
 const registerUser = new Promise((resolve, reject) => {
@@ -198,38 +182,68 @@ const registerUser = new Promise((resolve, reject) => {
             return response.json()
         })
         .then(data => resolve(data))
-        .catch(erorr => reject(error))
-})
-
+        .catch(error => reject(error))
+});
 
 registerUser.then(
     value => console.log(value),
-    error => console.error(error),
+    error => console.error(error)
 );
-
 
 /**
  * PUT request
  */
 
+const updateUserUrl = 'https://reqres.in/api/users/2';
+const updatedUser = {
+    "name": "morpheus",
+    "job": "zion resident"
+}
 
-//de copiat de la Sergiu din origin
+const updateUserUrlConfig = {
+    method: 'PUT',
+    headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': 'reqres-free-v1'
+    },
+    body: JSON.stringify(updatedUser),
+}
 
+const updateUser = new Promise((resolve, reject) => {
+    fetch(updateUserUrl, updateUserUrlConfig) // pana in punctul asta se face requestul
+        .then(response => {
+            return response.json()
+        })
+        .then(data => resolve(data))
+        .catch(error => reject(error))
+});
 
-
-
-// - pana aici
-
-
+updateUser.then(
+    value => console.log(value),
+    error => console.error(error)
+);
 
 /**
  * DELETE request
  */
 
 const deleteUserUrlHardCoded = 'https://reqres.in/api/users/2';
-const baseUrl = 'https://reqres.in/api/'
+const baseUrl = 'https://reqres.in/api/';
 const deleteUserId = '2';
-const deleteUserUrl = `${baseUrl}users/${deleteUserId}`
+const deleteUserUrl = `${baseUrl}users/${deleteUserId}`;
 
+const deleteUserUrlConfig = {
+    method: 'DELETE',
+    headers: {
+        'x-api-key': 'reqres-free-v1'
+    },
+}
 
-//copiaza codul
+const deleteUser = new Promise( (resolve, reject) => {
+    fetch(deleteUserUrl, deleteUserUrlConfig)
+    .then( response => {
+        if (response.status !== 204) {
+            throw 'Ai o eroare la stergearea userului'
+        }
+    })
+});
