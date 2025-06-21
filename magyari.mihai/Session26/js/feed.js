b25d7cf470510f7d1d14b7c35d4852932b6f8c21
magyarimihai <magyari.mihaly15@gmail.com>
2025-06-21 14:39:51 +0300

    modified session 26

--- magyari.mihai/Session26/js/feed.js
+++ magyari.mihai/Session26/js/feed.js
@@ -1,8 +1,8 @@
-const accountSettingsButton = document.getElementById('accountSettingsButton');
+const accountSettingsButton = document.getElementById('accountSettingsButton')
 const accountSettingsMenu = document.getElementsByClassName('accountSettingsMenu')[0];
 const logoutButton = document.getElementById('logoutButton');
 
-accountSettingsButton.addEventListener('click',function() {
+accountSettingsButton.addEventListener('click', function() {
     if (accountSettingsMenu.style.display === 'flex') {
         accountSettingsMenu.style.display = 'none';
     } else {
@@ -10,9 +10,58 @@ accountSettingsButton.addEventListener('click',function() {
     }
 });
 
-logoutButton.addEventListener('click', function () {
-    window.open('../login.html', '_self')
+logoutButton.addEventListener('click', function() {
+    window.open('../login.html', '_self');
 })
 
 const noOfLikesElem = document.getElementById('likesNumber');
-const noOfSharesElem = document.getElementById
+const noOfSharesElem = document.getElementById('sharesNumber');
+
+function getNumberOfLikes() {
+    noOfLikesElem.innerText = noOfLikesElem.innerText || 21;
+}
+
+function getNumberOfShares() {
+    noOfSharesElem.innerText = noOfSharesElem.innerText || 17;
+}
+
+function getData() {
+    getNumberOfLikes();
+    getNumberOfShares();
+}
+
+getData();
+
+const likeButton = document.getElementById('likeButton');
+const shareButton = document.getElementById('shareButton');
+
+let isLiked = false;
+let isShared = false;
+
+likeButton.addEventListener('click', function() {
+    isLiked = !isLiked;
+
+    console.dir(noOfLikesElem)
+
+    // if (isLiked) {
+    //     noOfLikesElem.innerText = Number(noOfLikesElem.innerText) + 1
+    // } else {
+    //     noOfLikesElem.innerText = Number(noOfLikesElem.innerText) - 1
+    // }
+
+    noOfLikesElem.innerText = isLiked
+    ? Number(noOfLikesElem.innerText) + 1
+    : Number(noOfLikesElem.innerText) - 1
+    
+    this.classList.toggle('touched');
+});
+
+shareButton.addEventListener('click', function() {
+    isShared = !isShared;
+
+    noOfSharesElem.innerText = isShared
+    ? Number(noOfSharesElem.innerText) + 1
+    : Number(noOfSharesElem.innerText) - 1
+    
+    this.classList.toggle('touched');
+});
