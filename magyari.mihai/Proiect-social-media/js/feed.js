44f62c5c496606e1e4d2771828877d9bebbd5deb
magyarimihai <magyari.mihaly15@gmail.com>
2025-06-27 21:26:19 +0300

    now you can leave comments

--- magyari.mihai/Proiect-social-media/js/feed.js
+++ magyari.mihai/Proiect-social-media/js/feed.js
@@ -83,3 +83,75 @@ const post = document.getElementById('feedPost');
 hideBtn.addEventListener('click', () => {
     post.style.display = 'none';
 })
+
+
+const newCommentForm = document.getElementById('addNewComment');
+
+newCommentForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const commentText = document.getElementById('newCommentText').value;
+    console.log(commentText);
+
+    if (commentText !== "") {
+        const newComment = document.createElement('div');
+        newComment.innerHTML = `
+        <div class="userComments">
+
+                    <div class="userCommentsContent">
+
+                        <div class="profileUserComment">
+
+                            <a href="">
+                                <img src="https://i.pinimg.com/736x/24/84/70/248470199d7901dd9f5adbed7a6a3932.jpg" alt="profile picture"
+                                    class="profileImage">
+                            </a>
+
+                        </div>
+
+                        <div class="userCommentText">
+
+                            <span class="commentUserName">Tony Stark</span> <br>
+
+                            <p>${commentText}</p>
+
+
+                            <div class="emojiReaction">
+
+                                <!-- &#128514; -->
+                                <span title="352 reactions">0</span>
+                                <img src="https://cdn-icons-png.flaticon.com/128/11820/11820106.png" alt=""
+                                    title="Reactions">
+                                <img src="https://cdn-icons-png.flaticon.com/128/3128/3128313.png" alt=""
+                                    title="Reactions">
+                                <img src="https://cdn-icons-png.flaticon.com/128/6637/6637195.png" alt=""
+                                    class="laughEmoji" title="Reactions">
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="commentReaction">
+
+                        <strong title="Posted 3 hours ago">Just now</strong>
+                        <strong class="commentReactionButton">Like</strong>
+                        <strong class="commentReactionButton">Reply</strong>
+
+
+
+
+                    </div>
+
+                </div>
+
+        `
+
+
+        document.getElementById('myComment').appendChild(newComment);
+    }
+
+    document.getElementById('newCommentText').value = ''
+
+})
