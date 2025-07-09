418a3515f3047f1d9608444543a7ca5b2d9ce78b
MDani94 <ave_avenger@yahoo.com>
2025-07-09 19:00:34 +0300

    delete

--- marina.daniel/smart-home-app/src/components/ui/Light.jsx
+++
@@ -1,7 +0,0 @@
-import './Light.scss';
-
-const Light = ({lightsOn}) => {
-    return <div className={`lights ${lightsOn ? 'yellow' : ''}`}></div>
-}
-
-export default Light;
--- marina.daniel/smart-home-app/src/components/ui/Room.jsx
+++
@@ -1,7 +0,0 @@
-import './Room.scss';
-
-const Room = ({status}) => {
-    return <div className={`room`} style={{opacity: status}}></div>
-}
-
-export default Room;
