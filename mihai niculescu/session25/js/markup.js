6565e1e7ca1cd1a490c284669abdfe90189cabc3
mihainiculescu96 <mihainiculescu96@yahoo.com>
2025-07-14 18:51:55 +0300

    feat: initialize smart home app with React and Vite
    - Added package.json with dependencies and scripts for development
    - Created initial SVG assets for Vite and React logos
    - Implemented main application structure in App.jsx
    - Styled application components with SCSS
    - Developed UI components for lights, AC, and room status
    - Added features management with dynamic feature addition
    - Implemented form for adding new features
    - Configured Vite for development server

---
+++ mihai niculescu/session25/js/markup.js
@@ -0,0 +1,25 @@
+export function updateNewsList(markup) {
+    document.getElementById('articlesWrapper').innerHTML = markup;
+}
+
+export function createMarkup({ author, title, description, url, urlToImage }) {
+    return `
+    <div class="article-card">
+        <h2 class="article-title"> ${title} </h2>
+        <h3 class="article-author"> ${author} </h3>
+        <img src=${urlToImage} class="article-img">
+        <p class="article-description"> ${description} </p>
+        <a href=${url} class="article-link" target="_blank"> Read more </a>
+    </div>    
+    `;
+}
+
+
+const test = `
+<div class="article-card">
+        <h2 class="article-title"></h2>
+        <h3 class="article-description"></h3>
+        <img src="" class="article-img">
+        <p class="article-description">text placehoder</p>
+        <a href="" class="article-link" target="_blank"></a>
+    </div>`;
