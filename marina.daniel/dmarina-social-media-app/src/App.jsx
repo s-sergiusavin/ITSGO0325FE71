37b4b6d9e62b15e1ee7c89a7c1f2039b40573144
MDani94 <ave_avenger@yahoo.com>
2025-07-23 19:16:48 +0300

    last sess

---
+++ marina.daniel/dmarina-social-media-app/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Layout from './components/Layout'
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route />
+        </Routes>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
---
+++ marina.daniel/dmarina-social-media-app/src/components/Layout.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./Navigation";
+
+const Layout = () => {
+  return (
+    <>
+      <Navigation />
+      <div>Layout</div>
+    </>
+  );
+};
+
+export default Layout;
