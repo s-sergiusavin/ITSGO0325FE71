9c4a6222b26eb5804bdb8e3d1074e681dab7d484
Stefania-b9 <butcastefaniaraluca@gmail.com>
2025-07-28 16:54:33 +0300

    Merge branch 'main' into Stefania-Butca

--- Stefania.Butca/stefaniabutca-social-media-app/src/components/layout.jsx
+++ Stefania.Butca/stefaniabutca-social-media-app/src/components/layout.jsx
@@ -1,17 +1,12 @@
-const layout = () => {
-    return (
-        <div className="layout">
-        <header className="layout-header">
-            <h1>Social Media App</h1>
-        </header>
-        <main className="layout-main">
-            {/* Main content will go here */}
-        </main>
-        <footer className="layout-footer">
-            <p>&copy; 2023 Stefania Butca</p>
-        </footer>
-        </div>
-    );
-    }
+import Navigation from "./Navigation";
 
-    export default layout;
+const Layout = ({children}) => {
+    return (
+        <>
+            <Navigation/>
+            <main>{children}</main>
+        </>
+    )
+}
+
+export default Layout;
---
+++ Stefania.Butca/stefaniabutca-social-media-app/src/pages/feed/newsfeed/NewsFeed.jsx
@@ -0,0 +1,7 @@
+import styles from './NewsFeed.module.scss';
+
+const NewsFeed = () => {
+    return <div className={styles.mainPost}>NewsFeed</div>
+}
+
+export default NewsFeed;
