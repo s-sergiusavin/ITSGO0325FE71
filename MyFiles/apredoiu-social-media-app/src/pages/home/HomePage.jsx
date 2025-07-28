8111e5e8b8d57d34b20bfec83138f873074d17eb
AlexandraTKO <alexandraalina18@gmail.com>
2025-07-28 20:39:23 +0300

    session 36

--- MyFiles/apredoiu-social-media-app/src/pages/home/HomePage.jsx
+++ MyFiles/apredoiu-social-media-app/src/pages/home/HomePage.jsx
@@ -1,23 +1,22 @@
 import styles from './HomePage.module.scss';
 import useFetch from '../../hooks/useFetch';
 import NewsFeed from '../feed/newsfeed/NewsFeed';
-import LeftSide from '../feed/leftside/LeftSide';
-import RightSide from "../feed/rightside/RightSide";
+import LeftSide from '../feed/leftside/LeftSide'
+import RightSide from '../feed/rightside/RightSide'
 
 const HomePage = () => {
-  const posts = useFetch('https://jsonplaceholder.typicode.com/posts');
 
-  return (
-    <div className={styles.mainContainer}>
-      <aside><LeftSide /></aside>
-      <section>
-        {posts?.map(post => (
-          <NewsFeed postData={post} key={post.id} />
-        ))}
-      </section>
-      <aside><RightSide /></aside>
+    const posts = useFetch('https://jsonplaceholder.typicode.com/posts');
+
+    return <div className={styles.mainContainer}>
+        <aside><LeftSide/></aside>
+        <section className={styles.newsfeed}>
+            {posts?.map( post => {
+                return <NewsFeed postData={post} key={post.id}/>
+            })}
+        </section>
+        <aside><RightSide/></aside>
     </div>
-  );
-};
+}
 
 export default HomePage;
