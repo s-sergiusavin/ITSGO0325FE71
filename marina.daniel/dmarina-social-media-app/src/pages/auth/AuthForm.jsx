30cdc443d2fc50e19e0d6f902f0673fc62d48b96
MDani94 <ave_avenger@yahoo.com>
2025-07-30 20:55:57 +0300

    copy session 36 from main

---
+++ marina.daniel/dmarina-social-media-app/src/pages/auth/AuthForm.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import styles from './AuthForm.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from '../../redux/selectors';
+import { loginUser, registerUser } from '../../redux/slices/authSlice';
+
+const AuthForm = () => {
+    const [isLogin, setIsLogin] = useState(true);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const user = useSelector(selectUser)
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
+
+    const toggleAuthState = () => {
+        setIsLogin(prevState => !prevState)
+    }
+
+    const submitHandler = async (event) => {
+        event.preventDefault();
+        const payload = {
+            email: username,
+            password
+        }
+        if (isLogin) {
+            try {
+                await dispatch(loginUser(payload));
+                if (user.isAuthenticated) {
+                    navigate('/')
+                }
+            } catch (err) {
+                console.error(err);
+            }
+        } else {
+            try {
+                await dispatch(registerUser(payload));
+            } catch(err) {
+                console.error(err);
+            }
+        }
+    }
+
+    const actionIsNotLoading = (
+        <button>{isLogin ? 'Login' : 'Create new account'}</button>
+    );
+
+    return <div className={styles.auth}>
+        <h2>{isLogin ? 'Login' : 'Logout'}</h2>
+        <form onSubmit={submitHandler} autoComplete='off' noValidate>
+            <div className={styles.control}>
+                <label htmlFor="username">Username</label>
+                <input type="text" id='username' placeholder='Enter your username' required
+                    onChange={e => setUsername(e.target.value)} />
+            </div>
+
+            <div className={styles.control}>
+                <label htmlFor="password">Password</label>
+                <input type="password" id='password' placeholder='Enter your password'
+                    required onChange={e => setPassword(e.target.value)} />
+            </div>
+
+            <div className={styles.actions}>
+                {user.error && <p>Please try again...</p>}
+                {user.loading && <p>Sending request...</p>}
+                {!user.loading && actionIsNotLoading}
+                <button className={styles.toggle} onClick={toggleAuthState}>
+                    {isLogin ? 'Create new account' : 'Login with an existing account'}
+                </button>
+            </div>
+        </form>
+    </div>
+}
+
+export default AuthForm;
